820fa265309a4f2a7591d1101a92fda2b4413f12
Evan 🌟 Chiu <evan@evanchiu.com>
2022-05-23 01:55:07 -0400

    feat(wallet): calculating potential imperial favors

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,15 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RaidSummary.vue"),
   },
+  {
+    path: "/wallet/inventory/calculations/:gw2Token?",
+    name: "Wallet",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Wallet.vue"),
+  },
 ];
 
 const router = new VueRouter({
