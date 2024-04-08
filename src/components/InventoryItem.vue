548abf21805b91674bdf1ed5ea3ec5cb20fa0210
Evan 🌟 Chiu <evan@evanchiu.com>
2024-04-07 21:48:03 -0400

    feat(wallet): breaking out item sources and adding potential mystic clovers

---
+++ src/components/InventoryItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full">
+    <div class="text-gray-600">{{ item.total }} {{ item.name }}</div>
+    <ul class="pl-4 text-sm text-gray-500">
+      <li v-for="source in Object.keys(item.counts)" :key="source">
+        {{ item.counts[source] }} {{ source }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: { item: Object },
+};
+</script>
--- src/views/WalletView.vue
+++ src/views/WalletView.vue
@@ -23,15 +23,29 @@
         >
       </div>
     </div>
+    <div
+      class="w-full p-4 m-4 bg-purple-200 text-purple-600 border-purple-600 rounded-lg text-xl content-center"
+      v-if="loading"
+    >
+      ...loading...
+    </div>
     <div class="w-full flex-down px-2" v-if="accountName">
       <h1 class="text-xl md:text-3xl flex-initial w-full">{{ accountName }}</h1>
-      <h1 class="text-xl md:text-2xl flex-initial w-full">
-        {{ totalPotential }} Potential Imperial Favors
+      <h1 class="text-xl md:text-2xl flex-initial w-full mt-2">
+        {{ potentialImperialFavors }} Potential Imperial Favors
       </h1>
-      <ul class="w-full">
-        <li>{{ imperialFavors }} Imperial Favors</li>
-        <li v-for="writ in Object.values(writs)" :key="writ.id">
-          {{ writ.count }} {{ writ.name }}
+      <ul class="w-full pl-4">
+        <li class="text-gray-600">{{ imperialFavors }} Imperial Favors</li>
+        <li v-for="writId in WRIT_IDS" :key="writId">
+          <inventory-item :item="items[writId]"></inventory-item>
+        </li>
+      </ul>
+      <h1 class="text-xl md:text-2xl flex-initial w-full mt-2">
+        {{ potentialMysticClovers }} Potential Mystic Clovers
+      </h1>
+      <ul class="w-full pl-4">
+        <li v-for="mysticId in MYSTIC_ITEM_IDS" :key="mysticId">
+          <inventory-item :item="items[mysticId]"></inventory-item>
         </li>
       </ul>
     </div>
@@ -40,19 +54,34 @@
 
 <script>
 import axios from "axios";
+import InventoryItem from "../components/InventoryItem";
 
 const FAVOR_WALLET_ID = 68;
 const WRIT_IDS = [96680, 96533, 96561, 95692];
+const MYSTIC_CLOVER_ID = 19675;
+const CHEST_OF_LOYALTY_ID = 68326;
+const CHEST_OF_LEGENDARY_CRAFTING_ID = 68352;
+const MYSTIC_ITEM_IDS = [
+  MYSTIC_CLOVER_ID,
+  CHEST_OF_LOYALTY_ID,
+  CHEST_OF_LEGENDARY_CRAFTING_ID,
+];
+const ITEM_IDS = [...WRIT_IDS, ...MYSTIC_ITEM_IDS];
 
 export default {
   name: "WalletView",
+  components: { InventoryItem },
   data() {
     return {
       gw2TokenEntry: "",
-      imperialFavors: 0,
       accountName: "",
-      writs: {},
-      totalPotential: 0,
+      items: {},
+      imperialFavors: 0,
+      loading: true,
+      MYSTIC_ITEM_IDS: MYSTIC_ITEM_IDS,
+      WRIT_IDS: WRIT_IDS,
+      potentialImperialFavors: 0,
+      potentialMysticClovers: 0,
     };
   },
   computed: {
@@ -62,74 +91,129 @@ export default {
   },
   watch: {
     gw2Token: function () {
-      this.loadWallet();
+      this.loadAll();
     },
   },
   created() {
     if (this.gw2Token) {
-      this.loadWallet();
+      this.loadAll();
     }
   },
   methods: {
+    loadAll: async function () {
+      this.loading = true;
+      ITEM_IDS.forEach((itemId) => {
+        this.items[itemId] = { total: 0, counts: {} };
+      });
+      await Promise.all([
+        this.loadItemData(),
+        this.loadAccountName(),
+        this.loadWallet(),
+        this.loadSharedInventory(),
+        this.loadMaterials(),
+        this.loadBank(),
+        this.loadCharacterInventory(),
+      ]);
+      const items = this.items;
+      const writCount = WRIT_IDS.map((writId) => items[writId]).reduce(
+        (sum, item) => sum + item.total,
+        0,
+      );
+      this.potentialImperialFavors = this.imperialFavors + writCount * 5;
+      this.potentialMysticClovers =
+        this.items[MYSTIC_CLOVER_ID].total +
+        this.items[CHEST_OF_LOYALTY_ID].total * 7 +
+        this.items[CHEST_OF_LEGENDARY_CRAFTING_ID].total * 7;
+      this.loading = false;
+    },
+    loadAccountName: async function () {
+      this.accountName = (
+        await axios.get(
+          `https://api.guildwars2.com/v2/account?v=2019-02-21T00:00:00Z&access_token=${this.gw2Token}`,
+        )
+      ).data.name;
+    },
     loadWallet: async function () {
-      try {
-        // fetch account details, wallet, shared inventory, materials, characters
-        const [
-          accountResponse,
-          walletResponse,
-          sharedInventoryResponse,
-          materialsResponse,
-          writItemResponse,
-        ] = await Promise.all([
-          axios.get(
-            `https://api.guildwars2.com/v2/account?v=2019-02-21T00:00:00Z&access_token=${this.gw2Token}`,
-          ),
-          axios.get(
-            `https://api.guildwars2.com/v2/account/wallet?access_token=${this.gw2Token}`,
-          ),
-          axios.get(
-            `https://api.guildwars2.com/v2/account/inventory?access_token=${this.gw2Token}`,
-          ),
-          axios.get(
-            `https://api.guildwars2.com/v2/account/materials?access_token=${this.gw2Token}`,
-          ),
-          axios.get(
-            `https://api.guildwars2.com/v2/items?ids=${WRIT_IDS.join(
-              ",",
-            )}&lang=en`,
-          ),
-        ]);
-
-        this.accountName = accountResponse.data.name;
-        this.imperialFavors = walletResponse.data.find(
-          (entry) => entry.id === FAVOR_WALLET_ID,
-        ).value;
-        WRIT_IDS.forEach((writId) => {
-          this.writs[writId] = writItemResponse.data.find(
-            (entry) => entry.id === writId,
-          );
-          this.writs[writId].count = 0;
-        });
-        materialsResponse.data
-          .filter((item) => item != null)
-          .filter((item) => WRIT_IDS.includes(item.id))
-          .forEach((item) => {
-            this.writs[item.id].count += item.count;
-          });
-        sharedInventoryResponse.data
-          .filter((item) => item != null)
-          .filter((item) => WRIT_IDS.includes(item.id))
-          .forEach((item) => {
-            this.writs[item.id].count += item.count;
-          });
-        const writCount = Object.values(this.writs).reduce(
-          (sum, item) => sum + item.count,
-          0,
-        );
-        this.totalPotential = this.imperialFavors + writCount * 5;
-      } catch (e) {
-        throw new Error("Error reading from GW2 API", { cause: e });
+      const walletResponse = await axios.get(
+        `https://api.guildwars2.com/v2/account/wallet?access_token=${this.gw2Token}`,
+      );
+      // Set values from wallet
+      const walletFavors = walletResponse.data.find(
+        (entry) => entry.id === FAVOR_WALLET_ID,
+      );
+      if (walletFavors) {
+        this.imperialFavors = walletFavors.value;
       }
+    },
+    loadSharedInventory: async function () {
+      const sharedInventoryResponse = await axios.get(
+        `https://api.guildwars2.com/v2/account/inventory?access_token=${this.gw2Token}`,
+      );
+      this.catalogInventory(sharedInventoryResponse.data, "shared inventory");
+    },
+    loadMaterials: async function () {
+      const materialsResponse = await axios.get(
+        `https://api.guildwars2.com/v2/account/materials?access_token=${this.gw2Token}`,
+      );
+      this.catalogInventory(materialsResponse.data, "bank materials");
+    },
+    loadBank: async function () {
+      const bankResponse = await axios.get(
+        `https://api.guildwars2.com/v2/account/bank?access_token=${this.gw2Token}`,
+      );
+      this.catalogInventory(bankResponse.data, "bank");
+    },
+    loadCharacterInventory: async function () {
+      const charactersNamesResponse = await axios.get(
+        `https://api.guildwars2.com/v2/characters?access_token=${this.gw2Token}`,
+      );
+      await Promise.all(
+        charactersNamesResponse.data.map(async (name) => {
+          const uriName = encodeURIComponent(name);
+          const characterInventoryResponse = await axios.get(
+            `https://api.guildwars2.com/v2/characters/${uriName}/inventory?access_token=${this.gw2Token}`,
+          );
+          characterInventoryResponse.data.bags.forEach((bag) => {
+            if (!bag || !bag.inventory) {
+              return;
+            }
+            this.catalogInventory(bag.inventory, name);
+          });
+        }),
+      );
+    },
+    loadItemData: async function () {
+      const itemResponse = await axios.get(
+        `https://api.guildwars2.com/v2/items?ids=${ITEM_IDS.join(",")}&lang=en`,
+      );
+      itemResponse.data.forEach((item) => {
+        if (!this.items[item.id]) {
+          this.items[item.id] = { counts: {} };
+        }
+        this.items[item.id].name = item.name;
+      });
+    },
+    catalogInventory: async function (inventory, sourceName) {
+      inventory
+        .filter((item) => item != null && ITEM_IDS.includes(item.id))
+        .forEach((item) => {
+          // Skip cataloging if there are none
+          if (item.count == 0) {
+            return;
+          }
+          // Add in count
+          if (this.items[item.id].counts[sourceName]) {
+            this.items[item.id].counts[sourceName] += item.count;
+          } else {
+            this.items[item.id].counts[sourceName] = item.count;
+          }
+          // Update total for this item
+          this.items[item.id].total = Object.keys(
+            this.items[item.id].counts,
+          ).reduce((sum, source) => {
+            return sum + this.items[item.id].counts[source];
+          }, 0);
+        });
     },
   },
 };
